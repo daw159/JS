abce25691fe0c232a59c49820e49e8bca58a37de
Muhammad Dawood <mdawood78612@gmail.com>
2026-06-19 19:45:20 +0500

    Js basic done of the Vraiable and teh dataypes

--- L1.js
+++ L1.js
@@ -1,4 +1,29 @@
-fullname="Mian dawood";
-console.log(fullname);
-x=null;
-console.log(x);
+// fullname="Mian dawood";
+// console.log(fullname);
+// x=null;
+// console.log(x);
+// y=88.99;
+// isfalse=true;
+// a=undefined
+// console.log(a);
+// console.log(y);
+
+// var let and teh cons are the tre ekeywords
+
+// const student={
+//     name:"dawoood",
+//     age:19,
+//     issuccess:false
+// }
+
+// console.log(student);
+
+
+const product={
+    name:"Pen",
+    price:265,
+    rating :4,
+    offer:5
+}
+
+console.log(product);
