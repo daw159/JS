db0fc7fd56bb1dc3a3be1abcf36ffa1f9e78491f
daw159 <mdawood78612@gmail.com>
2026-06-24 15:39:12 +0500

    Adding the dom part 2 file

--- script.js
+++ script.js
@@ -28,15 +28,59 @@
 // console.log(document.body.firstChild)
 
 
-let con=document.getElementById("heading");
-console.dir(con.innerText);
+// let con=document.getElementById("heading");
+// console.dir(con.innerText);
 
-con.innerText=con.innerText+"from apna college";
+// con.innerText=con.innerText+"from apna college";
 
 
-let data=document.querySelectorAll(".box");
+// let data=document.querySelectorAll(".box");
 
-data[0].innerText=data[0].innerText+" PUCIT ";
-data[1].innerText=data[1].innerText+" FCIT ";
-data[2].innerText=data[2].innerText+" CSS ";
+// data[0].innerText=data[0].innerText+" PUCIT ";
+// data[1].innerText=data[1].innerText+" FCIT ";
+// data[2].innerText=data[2].innerText+" CSS ";
+
+
+// Dom part 2
+// let div=document.querySelector("div");
+// console.log(div);
+
+// let id=div.getAttribute("id");
+// console.log(id);
+
+// let name=div.getAttribute("name");
+// console.log(name);
+
+// let par=document.querySelector("p");
+// console.log(par.setAttribute("para","newclass"));
+
+// let div=document.querySelector("div");
+// div.style.backgroundColor="red";
+// div.style.visibility="hidden";
+
+// insert teh elements   firts creta and the then add
+
+let el=document.createElement("button");
+el.innerText="Click me";
+el.style.backgroundColor="red";
+el.style.color="white";
+// console.log(el.innerText);
+
+let div=document.querySelector("div");
+// // div.append(el);
+div.prepend(el);
+// // div.after(el);
+// div.before(el);
+
+
+let newheading=document.createElement("h1");
+newheading.innerHTML="<i>this is teh new one </i>";  
+
+
+let bod=document.querySelector("body");
+bod.prepend(newheading);
+
+
+let para=document.querySelector("p");
+para.remove();
 
